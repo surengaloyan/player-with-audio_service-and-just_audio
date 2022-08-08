51849947106b1f462de72184f6960cd069f919ca
Ara Periyan <periyan.ara@gmail.com>
2022-08-08 18:43:11 +0400

    add seek functionality

---
+++ android/app/src/main/kotlin/com/example/player2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.player2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/services/playlist_repository.dart
@@ -0,0 +1,30 @@
+abstract class PlaylistRepository {
+  Future<List<Map<String, String>>> fetchInitialPlaylist();
+  Future<Map<String, String>> fetchAnotherSong();
+}
+
+class DemoPlaylist extends PlaylistRepository {
+  @override
+  Future<List<Map<String, String>>> fetchInitialPlaylist(
+      {int length = 3}) async {
+    return List.generate(length, (index) => _nextSong());
+  }
+
+  @override
+  Future<Map<String, String>> fetchAnotherSong() async {
+    return _nextSong();
+  }
+
+  var _songIndex = 0;
+  static const _maxSongNumber = 6;
+
+  Map<String, String> _nextSong() {
+    _songIndex = (_songIndex % _maxSongNumber) + 1;
+    return {
+      'id': _songIndex.toString().padLeft(3, '0'),
+      'title': 'Song $_songIndex',
+      'album': 'SoundHelix',
+      'url': 'http://887797-katamoto.tmweb.ru/00$_songIndex.mp3',
+    };
+  }
+}
